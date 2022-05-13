90d8021f9aa658df1ae73ec7956ecf2ea4f505fb
Liam Shaw <liamjosephshaw@gmail.com>
2022-05-13 11:52:20 +0100

    Basic styling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './styles.css';
+import logo from './img/logo.png';
 
 import { getPokemon } from './pokeAPI';
 
@@ -6,9 +7,10 @@ import { getPokemon } from './pokeAPI';
 const container = document.querySelector("#container")
 
 // Title
-const title = document.createElement("h1");
+const title = new Image();
 title.classList.add("title");
-title.textContent = "Who's that Pokemon?!";
+// title.textContent = "Who's that Pokemon?!";
+title.src = logo;
 
 container.append(title);
 
@@ -16,6 +18,9 @@ container.append(title);
 // Pokemon image
 const pokemonSprite = document.createElement("img");
 pokemonSprite.classList.add("pokemon-image");
+
+// Hide the sprite
+pokemonSprite.classList.add("hidden");
 
 container.append(pokemonSprite);
 
@@ -33,5 +38,16 @@ pokemon.then(function(response) {
 const guessBox = document.createElement("input");
 guessBox.type = "text";
 guessBox.classList.add("guess-box");
+guessBox.placeholder = "Type your guess here"
 
 container.append(guessBox);
+
+guessBox.focus();
+
+// Submit guess button
+const submitGuessButton = document.createElement("button");
+submitGuessButton.classList.add("submit-guess");
+submitGuessButton.textContent = "Submit";
+
+
+container.append(submitGuessButton);
--- src/pokeAPI.js
+++ src/pokeAPI.js
@@ -1,6 +1,4 @@
 export async function getPokemon(name) {
-
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {mode: 'cors'})
     return await response.json();
-
 }
