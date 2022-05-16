d7101b0424fd7bbbf5ddd8edc1693410c3ba1a3f
Liam Shaw <liamjosephshaw@gmail.com>
2022-05-16 23:30:53 +0100

    Basic game loop

---
+++ src/Game.js
@@ -0,0 +1,73 @@
+import { getRandomPokemon } from './pokeAPI';
+
+import { changeDisplayedPokemon, increasePokemonSize, 
+         revealPokemon, setGuessBoxValue } from './UI.js';
+
+let currentPokemonName;
+
+let turnCounter = 0;
+
+function generatePokemon() {
+
+    const pokemon = getRandomPokemon();
+
+    pokemon.then(function(response) {
+
+        if (response) {
+            currentPokemonName = response.name;
+
+            changeDisplayedPokemon(response.sprites.front_default);
+        }
+    });
+}
+
+export const checkGuess = (guess) => {
+
+    if (guess === currentPokemonName) {
+        return true;
+    }
+    
+    return false;
+}
+
+
+export const newGame = () => {
+    // Set turnCounter to 0;
+    turnCounter = 0;
+
+    // Generate new random pokemon
+    generatePokemon();
+}
+
+export const game = (guess) => {
+
+    if (turnCounter < 5) {
+
+        turnCounter++;
+        increasePokemonSize(turnCounter);
+
+        if (checkGuess(guess)) {
+            revealPokemon();
+            increasePokemonSize();
+            return;
+        }
+
+        if (turnCounter == 5) {
+            // Show full size Pokemon
+            revealPokemon();
+            increasePokemonSize();
+            
+            // Change guessBox to be real value
+            setGuessBoxValue(currentPokemonName);
+
+            return;
+        }
+    }
+}
+
+// Next turn
+    // Increase size of sprite
+    // increment turn counter
+
+
+// Stop taking user input
---
+++ src/UI.js
@@ -0,0 +1,114 @@
+import logo from './img/logo.png';
+
+import { game } from './Game';
+
+// Get container
+const container = document.querySelector("#container")
+
+export const setupUI = () => {
+    // Title
+    const title = new Image();
+    title.classList.add("title");
+    // title.textContent = "Who's that Pokemon?!";
+    title.src = logo;
+
+    container.append(title);
+
+
+    // Pokemon image
+    const pokemonSprite = document.createElement("img");
+    pokemonSprite.classList.add("pokemon-image");
+    pokemonSprite.style.width = "10%";
+
+    // Hide the sprite
+    pokemonSprite.classList.add("hidden");
+
+    container.append(pokemonSprite);
+
+
+    // Guess input box
+    const guessBox = document.createElement("input");
+    guessBox.type = "text";
+    guessBox.classList.add("guess-box");
+
+    container.append(guessBox);
+}
+
+export const changeDisplayedPokemon = (src) => {
+    const pokemonSprite = document.querySelector(".pokemon-image");
+
+    pokemonSprite.src = src;
+}
+
+export const increasePokemonSize = (turn) => {
+
+    const pokemonSprite = document.querySelector(".pokemon-image");
+
+    console.log("Turn: " + turn);
+
+    switch(turn) {
+
+        case 1:
+            pokemonSprite.style.width = "20%";
+            break;
+
+        case 2:
+            pokemonSprite.style.width = "40%";
+            break;
+
+        case 3:
+            pokemonSprite.style.width = "60%";
+            break;
+
+        default:
+            pokemonSprite.style.width = "100%";
+            break;
+    }
+}
+
+export const revealPokemon = () => {
+    const pokemonSprite = document.querySelector(".pokemon-image");
+    pokemonSprite.classList.remove("hidden");
+}
+
+function getGuessBox() {
+    return document.querySelector(".guess-box");
+}
+
+export const setGuessBoxValue = (value) => {
+    getGuessBox().value = value;
+}
+
+
+// Keyboard
+const keyboard = document.querySelector(".keyboard");
+keyboard.addEventListener("click", handleMouseClick);
+
+function handleMouseClick(e) {
+    if (e.target.matches("[data-key]")) {
+        pressKey(e.target.dataset.key);
+        return;
+    }
+
+    if (e.target.matches("[data-enter]")) {
+        //submit guess
+        game(getGuessBox().value);
+    }
+
+    if (e.target.matches("[data-delete]")) {
+        // backspace
+        deleteChar(getGuessBox().value);
+    }
+}
+
+function pressKey(key) {
+    getGuessBox().value += key;
+}
+
+function updateGuessBox(guess) {
+    getGuessBox().value = guess;
+}
+
+function deleteChar(string) {
+    updateGuessBox(string.substring(0, string.length-1));
+}
